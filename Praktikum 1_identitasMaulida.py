d7ac950ab1fe709d52aff661904bf8618278b088
maulidamrshaa <maulidamarsha3@gmail.com>
2026-08-27 13:14:08 +0700

    Add files via upload

---
+++ Praktikum 1_identitasMaulida.py
@@ -0,0 +1,12 @@
+print("Praktikum 1 - Identitas Mahasiswa")
+nama = "Maulida Marsha Shahada"
+NIM = "2605060048"
+ttl = "Magelang, 31 Maret 2008"
+jk = "Perempuan"
+hobi = "berolahraga, mendengarkan lagu"
+print ("\nBiodata Diri")
+print (f"Nama\t\t\t\t\t: {nama}")
+print (f"NIM\t\t\t\t\t\t: {NIM}")
+print (f"Tempat, Tanggal Lahir\t: {ttl}")
+print (f"Jenis Kelamin\t\t\t: {jk}")
+print (f"Hobi\t\t\t\t\t: {hobi}")
